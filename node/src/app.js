3521aea695169e094161451cf3e28dc75031a041
sbu10 <sam.burton-laurie@skybettingandgaming.com>
2024-06-14 16:58:46 +0100

    initial commit

---
+++ node/src/app.js
@@ -0,0 +1,50 @@
+const { APIGatewayClient, GetStagesCommand, GetStageCommand, UpdateStageCommand, GetRestApisCommand } = require('@aws-sdk/client-api-gateway');
+const apiGatewayClient = new APIGatewayClient({ region: "eu-west-2" });
+
+const setStageLimits = async(restApiId, stageName) => {
+  const getStageCommand = new GetStageCommand({restApiId, stageName});
+  const stage = await apiGatewayClient.send(getStageCommand)
+
+  const updateStageCommand = new UpdateStageCommand({
+    restApiId,
+    stageName,
+    patchOperations: [{
+      op: 'replace',
+      path: '/*/*/throttling/burstLimit',
+      value: '0'
+    },
+      {
+        op: 'replace',
+        path: '/*/*/throttling/rateLimit',
+        value: '0'
+      },
+    ]
+  });
+  await apiGatewayClient.send(updateStageCommand)
+
+  console.log(stage + ' updated')
+}
+
+const disableApiGateways = async() => {
+  const getRestApisCommand = new GetRestApisCommand();
+  // const apiIds = (await apiGatewayClient.send(command)).items.map(a => a.id)
+  const apiIds = (await apiGatewayClient.send(getRestApisCommand)).items.map(a => a.id)
+
+  console.log("API Id's:" + apiIds)
+
+  await Promise.all(apiIds.map(async(restApiId) => {
+    console.log("restApiId: "+restApiId)
+    const getStagesCommand = new GetStagesCommand({restApiId})
+    const stages = (await apiGatewayClient.send(getStagesCommand)).item
+
+    console.log("stages : " + stages + "for api id: " + restApiId)
+    await Promise.all(stages.map(stage => setStageLimits(restApiId, stage.stageName)))
+  }))
+
+}
+
+exports.handler = async() => {
+  await disableApiGateways()
+};
+//
+// exports.handler = async () => (  console.log("hello world") );
