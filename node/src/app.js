5e1063cdd84ef3980f1c93c902ea51538dc209d6
samburt0n5 <sam.burtonlaurie@hotmail.co.uk>
2024-06-27 16:44:09 +0100

    updated fetch function to api call

--- node/src/app.js
+++ node/src/app.js
@@ -1,60 +1,97 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
+const { v4: uuidv4 } = require('uuid');
 
-function findParentOrSiblingWithSelector($, element, selector) {
-  let parent = $(element).parent();
-  if (parent.is(selector)) {
-    return parent;
-  }
-
-  let sibling = parent.prev();
-  if (sibling.is(selector)) {
-    return sibling;
-  }
-
-  while (parent.length && !parent.is(selector)) {
-    parent = parent.parent();
-    if (parent.is(selector)) {
-      return parent;
-    }
-
-    sibling = parent.prev();
-    if (sibling.is(selector)) {
-      return sibling;
-    }
-  }
-
-  return null;
+const calculateSeason = (date) => {
+  const seasonStartAugust = new Date(new Date().getFullYear(),7,1)
+  //e.g. needs to be 2324 for 2023/2024 season
+  return (date < seasonStartAugust) ? (date.getFullYear() - 1).toString().slice(2) + date.getFullYear().toString().slice(2) : date.getFullYear().toString().slice(2) + (date.getFullYear() + 1).toString().slice(2)
 }
 
-const getScoresData = async(date) => {
-  console.log("Getting scores data")
-  axios.get('https://www.bbc.com/sport/football/scores-fixtures/'+date)
-    .then(response => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const teamHomeDivs = $('.ssrcss-bon2fo-WithInlineFallback-TeamHome.e1efi6g53');
-      const teamAwayDivs = $('.ssrcss-nvj22c-WithInlineFallback-TeamAway.e1efi6g52');
-
-      let fixtures = [];
-
-      teamHomeDivs.each((i, elem) => {
-        const team1 = $(elem).find('span:first').text().trim();
-        const team2 = $(teamAwayDivs[i]).find('span:first').text().trim();
-        const dateElement = findParentOrSiblingWithSelector($, elem, 'div:has(h2.ssrcss-51z0b8-PrimaryHeading.ejnn8gi2)');
-        const date = dateElement.find('h2.ssrcss-51z0b8-PrimaryHeading.ejnn8gi2').text().trim();
-        fixtures.push({date, team1, team2});
-      });
-
-      console.log(fixtures);
+const sendFixturesData = async(fixtures) => {
+  console.log("Sending fixture data.")
+  return axios.post('https://hgpkzpc3vb.execute-api.eu-west-2.amazonaws.com/dev/fixtures/bulkImport', fixtures)
+    .then(function (response) {
+      console.log(response);
+      Promise.resolve()
     })
-    .catch(console.error);
-
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-getScoresData("2024-06-17")
+async function getFixturesForDate(date) {
+  let fixtures = [];
+  await fetchData(date.toISOString().split('T')[0]).then(fixtureList => {
+    fixtureList.forEach(item => {
+      fixtures.push(item);
+    });
+  });
+  return fixtures
+}
 
-exports.handler = async() => {
-  console.log("hi")
-  await getScoresData()
+async function getFixturesForNDays(startDate, numberOfDays) {
+  let fixtures = [];
+  const previousDay = new Date();
+  previousDay.setDate(startDate.getDate() -1);
+  await getFixturesForDate(previousDay).then(fixtureList => { fixtureList.forEach(item => { fixtures.push(item) }) });
+  for (let i = 0; i < numberOfDays; i++) {
+    const nextDay = new Date();
+    nextDay.setDate(startDate.getDate() + i);
+    await getFixturesForDate(nextDay).then(fixtureList => { fixtureList.forEach(item => { fixtures.push(item) }) });
+  }
+  return fixtures;
+}
+
+exports.handler = async(event, context) => {
+  const date = event.queryStringParameters.date
+  const numberOfDays = event.queryStringParameters.numberOfDays
+  const updateApi = (event.queryStringParameters.updateApi === "true");
+  const startDate = new Date()
+  let fixtures = date === "all" ? await getFixturesForNDays(startDate, Number(numberOfDays)) : await fetchData(date)
+  console.log("Fixture count: " + fixtures.length)
+  if(updateApi) { await sendFixturesData(fixtures) }
 };
+
+const fetchData = async(date) => {
+  const today = new Date()
+  const url = 'https://web-cdn.api.bbci.co.uk/wc-poll-data/container/sport-data-scores-fixtures?selectedEndDate='+ date +'&selectedStartDate='+ date +'&todayDate='+ today.toISOString().split('T')[0] +'&urn=urn:bbc:sportsdata:football:tournament-collection:collated&useSdApi=false';
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+
+    if (!data.eventGroups) {
+      throw new Error("Missing 'eventGroups' property in the response");
+    }
+
+    return data.eventGroups.flatMap(eventGroup =>
+      eventGroup.secondaryGroups?.flatMap(group =>
+        group.events?.map(event => ({
+          id: uuidv4(),
+          dateTime: event.date.isoDate+'T'+event.time.displayTimeUK,
+          club1: event.home?.fullName,
+          club2: event.away?.fullName,
+          club1Score: event.home?.score,
+          club2Score: event.away?.score,
+          competition: eventGroup.displayLabel,
+          providerId: event.id,
+          season: (Array("Premier League","Bundesliga","La Liga","Ligue 1").includes(eventGroup.displayLabel)) ? calculateSeason(new Date(event.date.isoDate)) : '',
+          goals: [
+            ...extractGoals(event.home?.actions || []).map(goal => ({ ...goal, teamId: 'A' })),
+            ...extractGoals(event.away?.actions || []).map(goal => ({ ...goal, teamId: 'B' })),
+          ],
+        })) || []
+      ) || []
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function extractGoals(actions) {
+  return actions.filter(action => action.actions && action.actions.some(nestedAction => nestedAction.type === 'Goal')) // Filter actions with nested goals
+    .flatMap(action => action.actions.filter(nestedAction => nestedAction.type === 'Goal') // Filter nested goals
+      .map(goal => ({id:uuidv4(), playerName: action.playerName, clockTime: goal.timeLabel.value }))); // Create goal objects
+}
+
+// const date = new Date()
+// fetchData(date.toISOString().split('T')[0]).then(data => console.log(data))
---
+++ node/test/app.spec.js
@@ -0,0 +1,13 @@
+//
+//
+// describe('app tests', () => {
+//   test('calculating season', async () => {
+//     const calculateSeason = require('../src/app');
+//     const date2324 = calculateSeason(new Date(2024,6,1))
+//     expect(date2324).toBe("2324")
+//     const date2425 = calculateSeason(new Date(2024,7,1))
+//     expect(date2425).toBe("2425")
+//     const date2526 = calculateSeason(new Date(2025,6,1))
+//     expect(date2526).toBe("2526")
+//   });
+// })
